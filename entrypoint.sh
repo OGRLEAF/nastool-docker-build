9c75f17d48255883087f02f1c6579367ca3b662c
ChengYe <666@orgleaf.com>
2024-02-03 01:10:43 +0800

    clean build

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ usermod -o -u "$PUID" app-user
 
 addgroup app-user root
 
+cp $(which chromedriver) $WEBDRIVER
+chown app-user:app-user $WEBDRIVER
 chown app-user:app-user -R /nastool-lite/server
 chown app-user:app-user -R $NATOOL_CONFIG_PATH
 
