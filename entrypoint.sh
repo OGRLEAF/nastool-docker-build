68186fbe439dffdd0b59c0f9c8aec99a4513753d
ChengYe <666@orgleaf.com>
2024-02-04 19:29:50 +0800

    增加文案输出

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,18 @@ envsubst "\${NGINX_PORT}" < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.co
 echo "以PUID=${PUID}，PGID=${PGID}的身份启动程序..."
 
 # 更改 nt userid 和 groupid
+echo "Configure app-user pid=$PGID uid=$PUID"
 groupmod -o -g "$PGID" app-user
 usermod -o -u "$PUID" app-user
 
 adduser app-user root
 
+echo "Migrating webdriver"
 cp $(which chromedriver) $WEBDRIVER
 chown app-user:app-user $WEBDRIVER
 # chown app-user:app-user -R /nastool-lite/server
-chown app-user:app-user -R $NATOOL_CONFIG_PATH
+# chown app-user:app-user -R $NATOOL_CONFIG_PATH
+echo "Ensure root permission"
 chown app-user:app-user -R /root
 
 echo "Start nginx..."
